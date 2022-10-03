c46b51f8f5c46d821027bc36e52f90ebb8dfe388
Øivind Stensrud <oivind@oivind.net>
2022-10-03 21:58:25 +0200

    Oppdatert turnus

--- resources/schedule.ts
+++ resources/schedule.ts
@@ -150,6 +150,489 @@ const walkingSchedule: WalkingScheduleType = {
   "40": {
     erFerieUke: true,
   },
+  "41": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Theo",
+        familie2: "Alfred",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "42": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Sonja",
+        familie2: "Steinar",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Kristian",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "43": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Thea",
+        familie2: "Theo",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Alfred",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "44": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Olivia",
+        familie2: "Sonja",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Kristian",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "45": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Theo",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "46": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Theo",
+        familie2: "Alfred",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "47": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Sonja",
+        familie2: "Steinar",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Kristian",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "48": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Thea",
+        familie2: "Theo",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Alfred",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "49": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Olivia",
+        familie2: "Sonja",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Kristian",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "50": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Theo",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "51": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Theo",
+        familie2: "Alfred",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "1": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Sonja",
+        familie2: "Steinar",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Kristian",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "2": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Thea",
+        familie2: "Theo",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Alfred",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "3": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Olivia",
+        familie2: "Sonja",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Kristian",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "4": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Theo",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "5": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Theo",
+        familie2: "Alfred",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "6": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Sonja",
+        familie2: "Steinar",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Kristian",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "7": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Thea",
+        familie2: "Theo",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Alfred",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "8": {
+    erFerieUke: true,
+  },
+  "9": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Olivia",
+        familie2: "Sonja",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Kristian",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
+  "10": {
+    dager: [
+      {
+        familie1: "Sonja",
+        familie2: "Olivia",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Steinar",
+        familie2: "Theo",
+      },
+      {
+        familie1: "Kristian",
+        familie2: "Thea",
+      },
+      {
+        familie1: "Alfred",
+        familie2: "Theo",
+      },
+    ],
+  },
 };
 
 export { walkingSchedule };
