1e6f55ac1b5c6e675ef1f0b1caa259d5ea9d745e
Øivind Stensrud <oivind@oivind.net>
2022-11-06 11:53:54 +0100

    Styling på hideaway

--- components/hideaway/Hideaway.tsx
+++ components/hideaway/Hideaway.tsx
@@ -1,4 +1,5 @@
 import { ReactElement, useState } from "react";
+import { ExpandMore } from "../Assets";
 
 interface HideawayProps {
   title: string | ReactElement;
@@ -11,10 +12,12 @@ const Hideaway = (props: HideawayProps) => {
   const [visInnhold, toggleVisInnhold] = useState<boolean>(
     props.isOpen ?? false
   );
+  const classes = `hideaway ${props.classNames}`;
   return (
-    <section className={props.classNames}>
+    <section className={classes}>
       <button type={"button"} onClick={() => toggleVisInnhold(!visInnhold)}>
         {props.title}
+        <ExpandMore />
       </button>
       {visInnhold && props.children}
     </section>
