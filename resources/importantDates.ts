e66a01efc368fe1f70b27d05d19ec744a0354df7
Øivind Stensrud <oivind@oivind.net>
2022-11-27 12:42:41 +0100

    Ny turdag

--- resources/importantDates.ts
+++ resources/importantDates.ts
@@ -7,6 +7,12 @@ const importantDates: ImportantDateType[] = [
     description:
       "Vi skal grille. Ta med pølser/toast i aluminiumsfolie, grillspyd De som har, tar med en vedkubbe.",
   },
+  {
+    date: new Date("2022-12-01"),
+    title: "Tur med fadderene",
+    description:
+      "1AC til Helgerudskogen. 1BD til den store steinen. Vi lager bål. Ta med grillmat og grillpille. Husk sitteunderlag. Fint hvis de som har mulighet tar med vedkubbe.",
+  },
 ];
 
 export { importantDates };
