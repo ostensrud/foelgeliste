f7b614ac8a15332f8038363985629e530e125cc7
Øivind Stensrud <oivind@oivind.net>
2022-12-01 09:16:02 +0100

    :moyai: Retter skrivefeil.

--- resources/importantDates.ts
+++ resources/importantDates.ts
@@ -11,7 +11,7 @@ const importantDates: ImportantDateType[] = [
     date: new Date("2022-12-01"),
     title: "Tur med fadderene",
     description:
-      "1AC til Helgerudskogen. 1BD til den store steinen. Vi lager bål. Ta med grillmat og grillpille. Husk sitteunderlag. Fint hvis de som har mulighet tar med vedkubbe.",
+      "1AC til Helgerudskogen. 1BD til den store steinen. Vi lager bål. Ta med grillmat og grillpinne. Husk sitteunderlag. Fint hvis de som har mulighet tar med vedkubbe.",
   },
 ];
 
