7a3f949fe5230e80d5edeb2c987b02f04c4d95dc
Øivind Stensrud <oivind@oivind.net>
2022-12-11 19:03:40 +0100

    :moyai: Lucia-dagen

--- resources/importantDates.ts
+++ resources/importantDates.ts
@@ -13,6 +13,12 @@ const importantDates: ImportantDateType[] = [
     description:
       "1AC til Helgerudskogen. 1BD til den store steinen. Vi lager bål. Ta med grillmat og grillpinne. Husk sitteunderlag. Fint hvis de som har mulighet tar med vedkubbe.",
   },
+  {
+    date: new Date("2022-12-13"),
+    title: "Lucia-feiring",
+    description:
+      "Tirsdag 13. desember skal 1. trinn gå i Lucia-tog. Da ønsker vi at barna kler seg i hvitt. Skolen stiller med lys og glitter. Dere trenger ikke å gå til innkjøp av noe nytt tøy, en stor hvit t-skjorte er også fint.",
+  },
 ];
 
 export { importantDates };
