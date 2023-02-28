e5ee8eeed583aa5c1af0aae589c0efab09b63dd0
Øivind Stensrud <oivind@oivind.net>
2023-02-28 21:38:22 +0100

    :moyai: Ikke vis tidligere aar i komplett oversikt

--- components/completelist/CompleteList.tsx
+++ components/completelist/CompleteList.tsx
@@ -1,4 +1,4 @@
-import { getISOWeek } from "date-fns";
+import { getISOWeek, getYear } from "date-fns";
 import { walkingSchedule } from "../../resources/schedule";
 import { WeekRow } from "./weekrow";
 
@@ -6,25 +6,28 @@ const CompleteList = () => {
   const plan = walkingSchedule;
   const aarsliste = Object.keys(plan);
   const innevaerendeUke = getISOWeek(new Date());
+  const innevaerendeAar = getYear(new Date());
 
   return (
     <main className={"komplett"}>
-      {aarsliste.map((aar) => {
-        return (
-          <section key={aar}>
-            <h2>{aar}</h2>
-            {Object.keys(plan[aar]).map((ukenummer) => (
-              <WeekRow
-                aar={aar}
-                ukenummer={ukenummer}
-                key={aar + "::" + ukenummer}
-                innevaerendeUke={innevaerendeUke.toString()}
-                walkingSchedule={walkingSchedule}
-              />
-            ))}
-          </section>
-        );
-      })}
+      {aarsliste
+        .filter((aar) => Number.parseInt(aar, 10) >= innevaerendeAar)
+        .map((aar) => {
+          return (
+            <section key={aar}>
+              <h2>{aar}</h2>
+              {Object.keys(plan[aar]).map((ukenummer) => (
+                <WeekRow
+                  aar={aar}
+                  ukenummer={ukenummer}
+                  key={aar + "::" + ukenummer}
+                  innevaerendeUke={innevaerendeUke.toString()}
+                  walkingSchedule={walkingSchedule}
+                />
+              ))}
+            </section>
+          );
+        })}
     </main>
   );
 };
