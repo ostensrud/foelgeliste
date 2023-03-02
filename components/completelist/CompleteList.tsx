128098d931fbc2377c4b7f5e11061b64de0167ec
Øivind Stensrud <oivind@oivind.net>
2023-03-02 22:29:09 +0100

    :moyai: ScrollIntoView paa komplett oversikt

--- components/completelist/CompleteList.tsx
+++ components/completelist/CompleteList.tsx
@@ -1,12 +1,25 @@
 import { getISOWeek, getYear } from "date-fns";
+import { useEffect, useRef } from "react";
 import { walkingSchedule } from "../../resources/schedule";
 import { WeekRow } from "./weekrow";
 
+type RefListType = {
+  [key: string]: HTMLDivElement | null;
+};
+
 const CompleteList = () => {
+  const refList = useRef<RefListType>({ noop: null });
+
   const plan = walkingSchedule;
   const aarsliste = Object.keys(plan);
   const innevaerendeUke = getISOWeek(new Date());
   const innevaerendeAar = getYear(new Date());
+  useEffect(() => {
+    refList.current[innevaerendeUke.toString()]?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }, [innevaerendeUke]);
 
   return (
     <main className={"komplett"}>
@@ -17,13 +30,20 @@ const CompleteList = () => {
             <section key={aar}>
               <h2>{aar}</h2>
               {Object.keys(plan[aar]).map((ukenummer) => (
-                <WeekRow
-                  aar={aar}
-                  ukenummer={ukenummer}
+                <div
                   key={aar + "::" + ukenummer}
-                  innevaerendeUke={innevaerendeUke.toString()}
-                  walkingSchedule={walkingSchedule}
-                />
+                  ref={(element: HTMLDivElement) => {
+                    refList.current[ukenummer] = element;
+                  }}
+                  style={{ scrollMargin: "55px" }} // hoeyde paa header + 4px
+                >
+                  <WeekRow
+                    aar={aar}
+                    ukenummer={ukenummer}
+                    innevaerendeUke={innevaerendeUke.toString()}
+                    walkingSchedule={walkingSchedule}
+                  />
+                </div>
               ))}
             </section>
           );
