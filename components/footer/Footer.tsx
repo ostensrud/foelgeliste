96dbd1ed62438ac55b0867d07da45f90faeb6d05
Øivind Stensrud <oivind@oivind.net>
2023-11-26 10:20:39 +0100

    :moyai: Lukk meny ved navigasjon

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useIsNarrowScreen } from "../../hooks/useIsNarrowScreen";
 import { Navigation } from "../Navigation";
 
 import styles from "./Footer.module.css";
+import { useRouter } from "next/router";
 
 const Footer = () => {
+  const router = useRouter();
   const [expanded, toggleExpanded] = useState<boolean>(false);
+  useEffect(() => {
+    if (expanded) {
+      toggleExpanded(false);
+    }
+  }, [router.pathname]);
+
   if (!useIsNarrowScreen()) {
     return null;
   }
+
   return (
     <footer className={styles.appFooter}>
       {expanded && <Navigation />}
